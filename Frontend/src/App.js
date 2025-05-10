3eb8718c5c8ceb6f23e710cf9c27c0fd6c734be4
D-il <163960309+D-il@users.noreply.github.com>
2025-05-10 14:09:43 +0530

    edit app.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -8,8 +8,10 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+
         <Route path="/" element={<Home />} />
         <Route path="/community" element={<Community />} />
+        
       </Routes>
     </BrowserRouter>
   );
